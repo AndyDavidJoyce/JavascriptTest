fcc9096479a3c46991e0b1dc7f2e81a891f4f39b
Andrew Joyce <andy@zeta.net>
2017-07-24 13:26:23 +0100

    [Mod]: Removed unused imports

--- app/src/main/java/com/andrewjoyce/javascripttest/MainActivity.kt
+++ app/src/main/java/com/andrewjoyce/javascripttest/MainActivity.kt
@@ -2,7 +2,6 @@ package com.andrewjoyce.javascripttest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import org.liquidplayer.javascript.JSContext
 
